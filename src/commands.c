381761b2cd3f5695b46fb476e19361d647ce6e0c
Cyril COLINET <cyrilcolinet.pro@gmail.com>
2018-01-22 00:39:06 +0100

    :tongue: Adding pid management

---
+++ src/commands.c
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2018
+** minishell1
+** File description:
+** commands functions
+*/
+
+# include "my.h"
+# include "minishell.h"
+
+void command_exit(char *buffer)
+{
+	if (str_equals(buffer, "exit"))
+		exit(0);
+}
+
+void command_basics(param_t *param)
+{
+	int i = 0;
+	int ret = execve(param->strcat, param->arg, param->env_copy);
+
+	if (ret == -1)
+		exit(84);
+}
