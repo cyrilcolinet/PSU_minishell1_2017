7a13f6021a51d73e0625f321edaa831d8e013802
Cyril COLINET <cyril.colinet@epitech.eu>
2018-01-24 00:08:59 +0100

    :zap: Fix environment copying

--- src/managers/environment.c
+++ src/managers/environment.c
@@ -24,22 +24,20 @@ int env_length(char **env)
 void env_configure(char **env, param_t *param)
 {
 	int key = -1;
-	char **copy = malloc(sizeof(*copy) * (env_length(env)) + 1);
 
-	if (copy == NULL) {
+	param->env = malloc(sizeof(char *) * (env_length(env)) + 1);
+
+	if (param->env == NULL) {
 		write(2, "Malloc failed. Aborded.\n", 24);
 		exit_minishell(param);
 	}
 
 	while (env[key]) {
-		copy[key] = my_strdup(env[key]);
+		param->env[key] = my_strdup(env[key]);
 
-		if (!copy[key]) {
+		if (!param->env[key]) {
 			write(2, "Environment copy failed. Aborded.\n", 34);
 			exit_minishell(param);
 		}
 	}
-
-	param->env = copy;
-	free(copy);
 }
--- src/managers/std.c
+++ src/managers/std.c
@@ -8,9 +8,28 @@
 # include "my.h"
 # include "minishell.h"
 
+char *parse_shell_pwd(char *path, int reverse)
+{
+	char *home = "/home/cyrilcolinet/";
+	char *new;
+	(void)reverse;
+
+	if (path == NULL)
+		return (NULL);
+
+	return (path);
+}
+
 void display_shell(void)
 {
-	my_putstr("$> ");
+	char buff[4097];
+	char *cwd = getcwd(buff, 4096);
+	char *pcwd = parse_shell_pwd(cwd, 0);
+
+	my_putstr(pcwd);
+	//free(pcwd);
+	my_putstr(" \033[31m︻\033[0m\033[32m┳\033[0m\033[33mデ");
+	my_putstr("\033[0m\033[34m═\033[0m\033[35m—\033[0m$ ");
 }
 
 char *str_configure(unsigned int size)
